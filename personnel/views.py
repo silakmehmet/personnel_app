7d80ee1814b6fb3f37081bed8a6a4c7598674133
silakmehmet <silakmehmet@gmail.com>
2024-07-16 22:27:00 +0300

    Permissions added

--- personnel/views.py
+++ personnel/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
 
 from .models import Department, Personnel
 from .serializers import DepartmentSerializer, PersonnelSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class DepartmentMVS(ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
 
 class PersonnelMVS(ModelViewSet):
     queryset = Personnel.objects.all()
     serializer_class = PersonnelSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
