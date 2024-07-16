adc55b10c5edac0cee981991b8a26d799a17355d
silakmehmet <silakmehmet@gmail.com>
2024-07-16 21:23:53 +0300

    Personnel count added to the Department serializer using FOO Set

--- personnel/serializers.py
+++ personnel/serializers.py
@@ -4,10 +4,15 @@ from .models import Department, Personnel
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    personnel_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Department
         fields = "__all__"
         read_only_fields = ("id",)
+
+    def get_personnel_count(self, obj):
+        return obj.personnel_set.count()
 
 
 class PersonnelSerializer(serializers.ModelSerializer):
